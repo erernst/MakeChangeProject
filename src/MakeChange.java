f6aee661052e2f5b9a6ee5e6cdfe36a118ab434c
Erik Ernst <erikrobeyernst@Eriks-MacBook-Pro.local>
2017-08-04 15:38:34 -0600

    First bits of code

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -3,10 +3,31 @@ import java.util.Scanner;
 public class MakeChange {
 
 	public static void main(String[] args) {
-		Scanner keyb = new Scanner(System.in);
 		
-	static int getInput(int[])
-
+	
 	}
+	
+	static int checkout() {
+			System.out.print("What is the price of the iten: ");
+		int price = getInput();
+		System.out.print("What did they pay: ");
+		int paid = getInput();
+		(price-paid) > 0
+		return price - paid;
+	}
+	
+	static int getInput() {
+		Scanner keyb = new Scanner(System.in);
+		double input = keyb.nextDouble();
+		int intput =  (int)(input)*100;
+		keyb.close();
+		return intput;
+		
+	
+	}
+	static int calc change() {
+		
+	}
+	
 
 }
