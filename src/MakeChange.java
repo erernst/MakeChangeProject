cc98f03a94f1dba522ec0960519f73b3152eb4dc
Erik Ernst <erikrobeyernst@Eriks-MacBook-Pro.local>
2017-08-04 15:17:08 -0600

    Added file

---
+++ src/MakeChange.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MakeChange {
+
+	public static void main(String[] args) {
+		Scanner keyb = new Scanner(System.in);
+		
+	static int getInput(int[])
+
+	}
+
+}
